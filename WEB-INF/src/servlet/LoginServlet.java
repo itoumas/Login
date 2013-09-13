c192005d849f5837f2a24ae3e48a6d81e7979483
itoumas <itoumas@systena.co.jp>
2013-09-13 18:23:45 +0900

    DBとの接続作業途中

--- WEB-INF/src/servlet/LoginServlet.java
+++ WEB-INF/src/servlet/LoginServlet.java
@@ -1,40 +1,77 @@
 package servlet;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.naming.InitialContext;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 public class LoginServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
+			throws ServletException, IOException {
 
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 
 		ServletContext sc = getServletContext();
 
-		PrintWriter out = response.getWriter();
+		Connection connection = null;
 
-		if((id.equals("itou")) && (password.equals("itou")))
-		{
-			String name = "伊藤";
+		try{
 
-			request.setAttribute("name", name);
-			response.setContentType("text/html; charset=utf-8");
-			RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/Welcome.jsp");
-			rd.forward(request, response);
+			Class.forName("com.mysql.jdbc.Driver");
 
-		}else{
+/*
+			//JNDI参照コンテキストを取得
+			InitialContext initCtx = new InitialContext();
 
-			RequestDispatcher rd = sc.getRequestDispatcher("/login.jsp");
-			response.setContentType("text/html; charset=utf-8");
-			rd.forward(request, response);
+			//Tomcatで管理されているDB接続をJNDI経由で取得
+			DataSource ds = (DataSource) initCtx.lookup("java:comp/env/jbdc/localDB");
+			connection = ds.getConnection();
+
+			log("DBに接続しました");
+*/
+			if((id.equals("itou")) && (password.equals("itou")))
+			{
+				String name = "伊藤";
+
+				request.setAttribute("name", name);
+				response.setContentType("text/html; charset=utf-8");
+				RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/Welcome.jsp");
+				rd.forward(request, response);
+
+			}else{
+
+				RequestDispatcher rd = sc.getRequestDispatcher("/login.jsp");
+				response.setContentType("text/html; charset=utf-8");
+				rd.forward(request, response);
+			}
+
+		} catch (Exception e){
+
+			throw new ServletException(e);
+
+		}finally{
+
+			try{
+				connection.close();
+
+				log("DBへの接続を閉じました");
+
+			}catch (SQLException e){
+
+				throw new ServletException(e);
+			}
 		}
 	}
 }
+
+
