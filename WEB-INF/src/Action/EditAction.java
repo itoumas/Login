d6c02b5441fedcb7fbca849c667c758aa95cb64b
itoumas <itoumas@systena.co.jp>
2013-09-20 15:10:43 +0900

    EditActionクラスを作成

--- WEB-INF/src/Action/EditAction.java
+++ WEB-INF/src/Action/EditAction.java
@@ -3,11 +3,7 @@ package Action;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 
 public class EditAction {
 
@@ -16,24 +12,18 @@ public class EditAction {
 	String pass = "systena";
 	String url = "jdbc:mysql://10.10.14.162:3306/systenaDB?useUnicode=true&characterEncoding=UTF-8";
 
-	//JSPから受け取ったIDとパスワード
-//	String id = request.getParameter("id");
-//	String user_id = request.getParameter("user_id");
-//	String name = request.getParameter("name");
-//	String password = request.getParameter("password");
-//	String btn = request.getParameter("btn");
-
-	public void edit() {
+	public void edit(String id, String user_id, String name, String password, String btn) {
 
 		Connection connection = null;
 
 		try{
-			//JDBCへロード
+			//JDBCをロード
 			Class.forName("com.mysql.jdbc.Driver");
 
 			//MySQLへアクセス
 			Connection con = DriverManager.getConnection(url, user, pass);
 
+			//データを削除処理
 			if(btn.equals("Delete")){
 
 				String query = "delete from USER where ID = ?";
@@ -43,11 +33,11 @@ public class EditAction {
 				//パラメータセット
 				pstmt.setString(1, id);
 
-				int rs = pstmt.executeUpdate();
+				pstmt.executeUpdate();
 
-	//			rs.close();
 				pstmt.close();
 
+			//データの追加処理
 			}else if(btn.equals("Insert")){
 
 				String query = "insert into USER(USER_ID, NAME, PASSWORD) values (?, ?, ?)";
@@ -59,11 +49,11 @@ public class EditAction {
 				pstmt.setString(2, name);
 				pstmt.setString(3, password);
 
-				int rs = pstmt.executeUpdate();
+				pstmt.executeUpdate();
 
-	//			rs.close();
 				pstmt.close();
 
+			//データの更新処理
 			}else if(btn.equals("Update")){
 
 				String query = "update USER set USER_ID = ? , NAME = ? , PASSWORD = ? where ID = ?";
@@ -76,34 +66,25 @@ public class EditAction {
 				pstmt.setString(3, password);
 				pstmt.setString(4, id);
 
-				int rs = pstmt.executeUpdate();
+				pstmt.executeUpdate();
 
-	//			rs.close();
 				pstmt.close();
 			}
 
-			//もとのページに戻る
-			request.setAttribute("name", userName);
-			response.setContentType("text/html; charset=utf-8");
-			RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/Welcome.jsp");
-			rd.forward(request, response);
+		}catch (Exception e){
 
-		} catch (Exception e){
-
-			throw new ServletException(e);
 
 		}finally{
-	/*
-			try{
-				connection.close();
 
-				log("DBへの接続を閉じました");
+		try{
+			if(connection != null){
+
+				connection.close();
+			}
 
 			}catch (SQLException e){
 
-				throw new ServletException(e);
 			}
-	*/
 		}
 	}
 }
--- WEB-INF/src/servlet/EditDate.java
+++ WEB-INF/src/servlet/EditDate.java
@@ -1,10 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -14,15 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import Action.EditAction;
+
 public class EditDate  extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		//MySQLにアクセスするためのユーザ名、パスワード、URL
-		String user = "systena";
-		String pass = "systena";
-		String url = "jdbc:mysql://10.10.14.162:3306/systenaDB?useUnicode=true&characterEncoding=UTF-8";
 
 		request.setCharacterEncoding("UTF-8");
 
@@ -32,104 +25,30 @@ public class EditDate  extends HttpServlet {
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
 		String btn = request.getParameter("btn");
-//		String loginName = request.getParameter("loginName");
-
-//		log("ログイン用:" + loginName);
 
 		ServletContext sc = getServletContext();
+
+		EditAction editAction = new EditAction();
+
+		editAction.edit(id, user_id, name, password, btn);
 
 		//セッションから名前を取り出す
 		HttpSession session = request.getSession(false);
 
+		//セッションが生成されていない状態で処理を行おうとした場合、ログイン画面へ飛ばす
 		if (session == null){
 			response.setContentType("text/html; charset=utf-8");
 			RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/login.jsp");
 			rd.forward(request, response);
 		}
 
+		//セッションに保存されたユーザ名をレスポンスと送るために取り出す
 		String userName = (String)session.getAttribute("userName");
 
-		log("セッション:" + userName);
-
-		Connection connection = null;
-
-		try{
-			//JDBCへロード
-			Class.forName("com.mysql.jdbc.Driver");
-
-			//MySQLへアクセス
-			Connection con = DriverManager.getConnection(url, user, pass);
-
-			if(btn.equals("Delete")){
-
-				String query = "delete from USER where ID = ?";
-
-				PreparedStatement pstmt = con.prepareStatement(query);
-
-				//パラメータセット
-				pstmt.setString(1, id);
-
-				int rs = pstmt.executeUpdate();
-
-//				rs.close();
-				pstmt.close();
-
-			}else if(btn.equals("Insert")){
-
-				String query = "insert into USER(USER_ID, NAME, PASSWORD) values (?, ?, ?)";
-
-				PreparedStatement pstmt = con.prepareStatement(query);
-
-				//パラメータセット
-				pstmt.setString(1, user_id);
-				pstmt.setString(2, name);
-				pstmt.setString(3, password);
-
-				int rs = pstmt.executeUpdate();
-
-//				rs.close();
-				pstmt.close();
-
-			}else if(btn.equals("Update")){
-
-				String query = "update USER set USER_ID = ? , NAME = ? , PASSWORD = ? where ID = ?";
-
-				PreparedStatement pstmt = con.prepareStatement(query);
-
-				//パラメータセット
-				pstmt.setString(1, user_id);
-				pstmt.setString(2, name);
-				pstmt.setString(3, password);
-				pstmt.setString(4, id);
-
-				int rs = pstmt.executeUpdate();
-
-//				rs.close();
-				pstmt.close();
-			}
-
-			//もとのページに戻る
-			request.setAttribute("name", userName);
-			response.setContentType("text/html; charset=utf-8");
-			RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/Welcome.jsp");
-			rd.forward(request, response);
-
-		} catch (Exception e){
-
-			throw new ServletException(e);
-
-		}finally{
-/*
-			try{
-				connection.close();
-
-				log("DBへの接続を閉じました");
-
-			}catch (SQLException e){
-
-				throw new ServletException(e);
-			}
-*/
-		}
+		//処理後もとのページに戻る
+		request.setAttribute("name", userName);
+		response.setContentType("text/html; charset=utf-8");
+		RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/Welcome.jsp");
+		rd.forward(request, response);
 	}
 }
