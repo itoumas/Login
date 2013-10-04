ce9ac52ea446a6bbd038f6254ba6222b09a9f42c
itoumas <itoumas@systena.co.jp>
2013-10-04 14:56:13 +0900

    ＤｅｌｅｔｅＴｅｓｔクラスを作成　テスト失敗

---
+++ test/DAO/DeleteTest.java
@@ -0,0 +1,14 @@
+package DAO;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeleteTest extends Delete {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
