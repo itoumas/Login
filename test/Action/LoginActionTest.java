a1ba03811ba2bb4bfe5dfdc3f57491a838409517
itoumas <itoumas@systena.co.jp>
2013-10-04 12:53:09 +0900

    LoiginActionTestクラスの作成

---
+++ test/Action/LoginActionTest.java
@@ -0,0 +1,14 @@
+package Action;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginActionTest extends LoginAction {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
---
+++ test/servlet/LoginServletTest.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class LoginServletTest {
+
+	@Test
+	public void test() throws Exception {
+		String test = "";
+	    MockHttpServletRequest request = new MockHttpServletRequest();
+	    MockHttpServletResponse response = new MockHttpServletResponse();
+
+	    LoginServlet servlet = new LoginServlet();
+	    servlet.doPost(request, response);
+	}
+
+}
