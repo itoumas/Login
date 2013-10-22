dce003cfc5ccadc52beb6ed1e61aef0661bf9a31
itoumas <itoumas@systena.co.jp>
2013-10-22 21:19:07 +0900

    LoginServletTest完成

--- test/servlet/LoginServletTest.java
+++ test/servlet/LoginServletTest.java
@@ -11,29 +11,42 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+
 public class LoginServletTest {
 
+
+	//モックオブジェクトを格納します。
+	private LoginServlet loginServlet = null;
+	private MockHttpServletRequest request = null;
+	private MockHttpServletResponse response = null;
+	private ServletContext servletContext = null;
+	private RequestDispatcher requestDispacher = null;
+
+
+	/**
+	 * 各テスト前にモックオブジェクトを作成します。
+	 */
 	@Before
 	public void setUp() {
 
+		//LoginServletをspyでオブジェクト化します。
+		loginServlet = spy(new LoginServlet());
 
+		//モックオブジェクトを作成します。
+		request = new MockHttpServletRequest();
+		response = new MockHttpServletResponse();
+		servletContext = mock(ServletContext.class);
+		requestDispacher = mock(RequestDispatcher.class);
 	}
 
 
+	/**
+	 * ログインが成功した場合のテスト
+	 *
+	 * @throws Exception
+	 */
 	@Test
 	public void testDoPostSuccess() throws Exception {
-
-		// ************ ログイン成功の場合 *************
-
-		//LoginServletをspyでオブジェクト化します。
-		LoginServlet loginServlet = spy(new LoginServlet());
-
-		//モックオブジェクトを作成します。
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		MockHttpServletResponse response = new MockHttpServletResponse();
-
-		ServletContext servletContext = mock(ServletContext.class);
-		RequestDispatcher requestDispacher = mock(RequestDispatcher.class);
 
 		//getServletContextメソッドが呼び出されたら強制的にServletContextモックオブジェクトを返します。
 		doReturn(servletContext).when(loginServlet).getServletContext();
@@ -42,7 +55,7 @@ public class LoginServletTest {
 		doReturn(requestDispacher).when(servletContext).getRequestDispatcher("/WEB-INF/Welcome.jsp");	//パスを変えるとテスト失敗
 
 		//リクエストにパラメータをセットします(DBに存在するIDとPASSWORD)。
-		request.setParameter("user_id", "itou_id");	//存在しないIDとPASSWORDを使用するとテスト失敗
+		request.setParameter("user_id", "itou_id");		//存在しないIDとPASSWORDを使用するとテスト失敗
 		request.setParameter("password", "itou_pass");
 
 		//doPostメソッドを実行します。
@@ -62,26 +75,19 @@ public class LoginServletTest {
 	}
 
 
+	/**
+	 * ログインが失敗した場合のテスト
+	 *
+	 * @throws Exception
+	 */
 	@Test
 	public void testDoPostFalse() throws Exception {
-
-		// ************ ログイン失敗の場合 *************
-
-		//LoginServletをspyでオブジェクト化します。
-		LoginServlet loginServlet = spy(new LoginServlet());
-
-		//モックオブジェクトを作成します。
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		MockHttpServletResponse response = new MockHttpServletResponse();
-
-		ServletContext servletContext = mock(ServletContext.class);
-		RequestDispatcher requestDispacher = mock(RequestDispatcher.class);
 
 		//getServletContextメソッドが呼び出されたら強制的にServletContextモックオブジェクトを返します。
 		doReturn(servletContext).when(loginServlet).getServletContext();
 
 		//getRequestDispacharが呼び出されたら強制的にRequestDispatcherモックオブジェクトを返します。
-		doReturn(requestDispacher).when(servletContext).getRequestDispatcher("/login.jsp");	//パスを変えるとテスト失敗
+		doReturn(requestDispacher).when(servletContext).getRequestDispatcher("/login.jsp");		//パスを変えるとテスト失敗
 
 		//リクエストにパラメータをセットします(DBに存在しないIDとPASSWORD)。
 		request.setParameter("user_id", "miss");	//存在するIDとPASSWORDを使用するとテスト失敗
